7e49c2666c26875b4300a3e13c6a0903aa0675e1
Shruti <130890404+shruti500@users.noreply.github.com>
2025-08-29 16:49:20 +0530

    Add files via upload

---
+++ sales dashboard outcome.py
@@ -0,0 +1,5 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/shrut/OneDrive/Documents/music trends/sales dashboard using bower bi.py
+Synthetic sales data saved as 'synthetic_sales_data.csv'
